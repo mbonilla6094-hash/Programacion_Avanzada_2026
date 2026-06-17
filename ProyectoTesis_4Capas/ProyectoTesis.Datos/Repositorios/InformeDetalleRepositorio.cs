6bde6ab47a91a0ef648bdfc8eca23c14ec4c9bd8
Mateo Bonilla <mateo@example.com>
2026-06-16 23:38:40 -0500

    Eliminar código: Repositorio exclusivo para documentación e imágenes

--- ProyectoTesis_4Capas/ProyectoTesis.Datos/Repositorios/InformeDetalleRepositorio.cs
+++
@@ -1,7 +0,0 @@
-namespace ProyectoTesis.Datos.Repositorios
-{
-    public class InformeDetalleRepositorio : RepositorioBase<InformeDetalle>
-    {
-        public InformeDetalleRepositorio(SistemaTesisEntities context) : base(context) { }
-    }
-}
--- ProyectoTesis_4Capas/ProyectoTesis.Logica/Interfaces/ILoginServicio.cs
+++
@@ -1,9 +0,0 @@
-using ProyectoTesis.Entidades;
-
-namespace ProyectoTesis.Logica.Interfaces
-{
-    public interface ILoginServicio
-    {
-        Profesor Autenticar(string usuario, string contrasena);
-    }
-}
